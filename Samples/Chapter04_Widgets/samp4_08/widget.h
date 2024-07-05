0af47f3902991c4baaa14d8e21fde30be16d36dd
Goodenough <2323342501@qq.com>
2024-07-05 10:21:57 +0800

    add chapter04 samp4_08 QTimer和QElapsedTimer

---
+++ Samples/Chapter04_Widgets/samp4_08/widget.h
@@ -0,0 +1,38 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QTimer>
+#include <QElapsedTimer>
+#include <QTime>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Widget;
+}
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+private:
+    QTimer *m_timer; // 定时器
+    QElapsedTimer m_counter; // 计时器
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+private slots:
+    void do_timerTimeout(); // 自定义槽函数，与定时器的timeout()信号关联
+    void do_timer_shot(); // 与单次定时器关联的槽函数
+
+private slots:
+    void on_btnStart_clicked();
+
+    void on_btnStop_clicked();
+
+    void on_btnOneShot_clicked();
+
+private:
+    Ui::Widget *ui;
+};
+#endif // WIDGET_H
