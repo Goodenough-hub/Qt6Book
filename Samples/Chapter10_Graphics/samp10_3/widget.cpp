5c910a1ac951b33cd71583c00dc8b2d531a5a3be
Goodenough <2323342501@qq.com>
2024-07-25 19:03:49 +0800

    samp10_3 添加渐变填充和叠加效果

--- Samples/Chapter10_Graphics/samp10_3/widget.cpp
+++ Samples/Chapter10_Graphics/samp10_3/widget.cpp
@@ -36,6 +36,19 @@ void Widget::paintEvent(QPaintEvent *event)
     pen.setJoinStyle(Qt::BevelJoin);
     painter.setPen(pen);
 
+    // 线性渐变
+    QLinearGradient linearGrad(0, 0, 100, 0); // 从左到右
+    linearGrad.setColorAt(0, Qt::yellow); // 起点颜色
+    linearGrad.setColorAt(1, Qt::green); // 终点颜色
+    linearGrad.setSpread(QGradient::PadSpread); // 展布模式
+    painter.setBrush(linearGrad);
+
+    // 设置组合模式
+    painter.setCompositionMode(QPainter::CompositionMode_Difference);
+    // painter.setCompositionMode(QPainter::RasterOp_NotSourceXorDestination);
+    // painter.setCompositionMode(QPainter::CompositionMode_Exclusion);
+
+
     for(int i = 0; i < 36; i ++)
     {
         painter.drawEllipse(QPoint(50, 0), 50, 50);
