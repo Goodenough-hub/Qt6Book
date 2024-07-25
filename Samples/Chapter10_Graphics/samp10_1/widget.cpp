7cb2d34f7ca3a5cc10bed27239c7cfbfc9c8df46
Goodenough <2323342501@qq.com>
2024-07-25 14:45:48 +0800

    samp10_1 辐射渐变：径向渐变，焦点不同

--- Samples/Chapter10_Graphics/samp10_1/widget.cpp
+++ Samples/Chapter10_Graphics/samp10_1/widget.cpp
@@ -23,10 +23,10 @@ void Widget::paintEvent(QPaintEvent *event)
     int H = this->height(); // 绘图区高度
     QRect rect(W/4, H/4, W/2, H/2); // 中间区域矩形
     painter.setPen(QPen(Qt::NoPen)); // 设置画笔，不显示线条
-    // 线性渐变
-    QRadialGradient radialGrad(W/2, H/2, qMax(W/3, H/3), W/2, H/2);
+    // 径向渐变，焦点不同
+    QRadialGradient radialGrad(W/2, H/2, W/2, 3 * H/4, H/2);
     radialGrad.setColorAt(0, Qt::white); // 起点颜色
-    radialGrad.setColorAt(1, Qt::blue); // 终点颜色
+    radialGrad.setColorAt(0.8, Qt::blue); // 终点颜色
     painter.setBrush(radialGrad);
     painter.drawRect(rect); // 绘制矩形
     event->accept();
