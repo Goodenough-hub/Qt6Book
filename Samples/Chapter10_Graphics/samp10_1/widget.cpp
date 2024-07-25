b87f64783f6905e7178a1e34bcb71506e578d7fc
Goodenough <2323342501@qq.com>
2024-07-25 14:53:23 +0800

    samp10_1 圆锥形渐变

--- Samples/Chapter10_Graphics/samp10_1/widget.cpp
+++ Samples/Chapter10_Graphics/samp10_1/widget.cpp
@@ -23,11 +23,11 @@ void Widget::paintEvent(QPaintEvent *event)
     int H = this->height(); // 绘图区高度
     QRect rect(W/4, H/4, W/2, H/2); // 中间区域矩形
     painter.setPen(QPen(Qt::NoPen)); // 设置画笔，不显示线条
-    // 径向渐变，焦点不同
-    QRadialGradient radialGrad(W/2, H/2, W/2, 3 * H/4, H/2);
-    radialGrad.setColorAt(0, Qt::white); // 起点颜色
-    radialGrad.setColorAt(0.8, Qt::blue); // 终点颜色
-    painter.setBrush(radialGrad);
+    // 圆锥形渐变
+    QConicalGradient coniGrad(W/2, H/2, 45);
+    coniGrad.setColorAt(0, Qt::blue); // 起点颜色
+    coniGrad.setColorAt(0.8, Qt::white); // 终点颜色
+    painter.setBrush(coniGrad);
     painter.drawRect(rect); // 绘制矩形
     event->accept();
 }
