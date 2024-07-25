5e465ea301f876b018ab42be95992f43dd3a12bc
Goodenough <2323342501@qq.com>
2024-07-25 14:30:18 +0800

    samp10_1 线性渐变

--- Samples/Chapter10_Graphics/samp10_1/widget.cpp
+++ Samples/Chapter10_Graphics/samp10_1/widget.cpp
@@ -19,25 +19,16 @@ Widget::~Widget()
 void Widget::paintEvent(QPaintEvent *event)
 {
     QPainter painter(this); // 创建QPainter对象
-    painter.setRenderHint(QPainter::Antialiasing);
     int W = this->width(); // 绘图区宽度
     int H = this->height(); // 绘图区高度
     QRect rect(W/4, H/4, W/2, H/2); // 中间区域矩形
-    // 设置画笔
-    QPen pen;
-    pen.setWidth(3); // 线宽
-    pen.setColor(Qt::red); // 线条颜色
-    pen.setStyle(Qt::SolidLine); // 线条样式
-    pen.setCapStyle(Qt::FlatCap); // 线条端点样式
-    pen.setJoinStyle(Qt::BevelJoin); // 线条的连接样式
-    painter.setPen(pen);
-    // 设置画刷
-    QPixmap texturePixmap(":/Pics/images/texture.jpg");
-    QBrush brush;
-    brush.setStyle(Qt::TexturePattern); // 画刷填充样式
-    brush.setTexture(texturePixmap); // 设置材质图片
-    painter.setBrush(brush);
-    // 绘图
+    painter.setPen(QPen(Qt::NoPen)); // 设置画笔，不显示线条
+    // 线性渐变
+    QLinearGradient linearGrad(rect.left(), rect.top(), rect.right(), rect.top()); // 从左到右
+    linearGrad.setColorAt(0, Qt::blue); // 起点颜色
+    linearGrad.setColorAt(0.5, Qt::white); // 中间点颜色
+    linearGrad.setColorAt(1, Qt::blue); // 终点颜色
+    painter.setBrush(linearGrad);
     painter.drawRect(rect); // 绘制矩形
     event->accept();
 }
